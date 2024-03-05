8e1bca9f0d4fd8ea141519b73c81a9a5c9d208e9
Issac-31 <issacnewton934@gmail.com>
2024-03-05 18:34:34 +0530

    yes

--- Backend/models/message.model.js
+++ Backend/models/message.model.js
@@ -1,25 +1,26 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-    {
-        senderId :{
-            type :mongoose.Schema.Types.ObjectId,
-            ref :"User",
-            required :true,
-        },
-        receiverId :{
-            type :mongoose.Schema.Types.ObjectId,
-            ref :"User",
-            required :true,
-        },
-        message:{
-            type :String,
-            required :true,
-        },
-    },
-    //CreatedAt ,updatedAt => message.createdAt : 15:30
-    {timestamps :true}
+	{
+		senderId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		receiverId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+		},
+		// createdAt, updatedAt
+	},
+	{ timestamps: true }
 );
 
-const Message = mongoose.model("Message" ,messageSchema);
+const Message = mongoose.model("Message", messageSchema);
+
 export default Message;
